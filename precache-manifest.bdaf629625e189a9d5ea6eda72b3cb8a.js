b91139eb172652a0c8ea6ec32f42504e58f49e5f
Logvinenko-web <logvinenko.elit@gmail.com>
2020-10-26 12:56:04 +0200

    Updates

---
+++ precache-manifest.bdaf629625e189a9d5ea6eda72b3cb8a.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597aa702739d1d03c9738d3a39a526fa",
+    "url": "/CheckboxDemo/index.html"
+  },
+  {
+    "revision": "1914a8cbfce8fb2d83d1",
+    "url": "/CheckboxDemo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e06440f7191b480575b2",
+    "url": "/CheckboxDemo/static/css/main.e1e0bf7d.chunk.css"
+  },
+  {
+    "revision": "1914a8cbfce8fb2d83d1",
+    "url": "/CheckboxDemo/static/js/2.3d5b54c0.chunk.js"
+  },
+  {
+    "revision": "48ac8ec662c878179d9a66a3836a4b97",
+    "url": "/CheckboxDemo/static/js/2.3d5b54c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06440f7191b480575b2",
+    "url": "/CheckboxDemo/static/js/main.76f8b2ea.chunk.js"
+  },
+  {
+    "revision": "35a42a08c5851a22705b",
+    "url": "/CheckboxDemo/static/js/runtime-main.b5002feb.js"
+  },
+  {
+    "revision": "d3352bbd991bbf05ecc9ec78482ddb8b",
+    "url": "/CheckboxDemo/static/media/Organiz.d3352bbd.png"
+  },
+  {
+    "revision": "cbbc879c26c7b7fbaa37c49532a4e03b",
+    "url": "/CheckboxDemo/static/media/archivCheck.cbbc879c.svg"
+  },
+  {
+    "revision": "a6e356df67630a0941455ee1e4383f7b",
+    "url": "/CheckboxDemo/static/media/check.a6e356df.png"
+  },
+  {
+    "revision": "8e1ed12d9fbc1890aef5ac3f7b41b887",
+    "url": "/CheckboxDemo/static/media/checkReceipt.8e1ed12d.png"
+  },
+  {
+    "revision": "61f9c972b35509a05cfdf2a4b5fc71b6",
+    "url": "/CheckboxDemo/static/media/chenges.61f9c972.png"
+  },
+  {
+    "revision": "c96241bb4abb53e812f8c184fb4cafe5",
+    "url": "/CheckboxDemo/static/media/chenges.c96241bb.svg"
+  },
+  {
+    "revision": "1e7f0d9e17069d0bd949614ca0c16e4b",
+    "url": "/CheckboxDemo/static/media/company.1e7f0d9e.svg"
+  },
+  {
+    "revision": "2d05383f37e463fc8fb1f29de0f32f52",
+    "url": "/CheckboxDemo/static/media/companyS.2d05383f.png"
+  },
+  {
+    "revision": "42570a9fbd91d1e2f683a3f3f659235e",
+    "url": "/CheckboxDemo/static/media/e-Ukraine-Light.42570a9f.ttf"
+  },
+  {
+    "revision": "9fabd1e0a380b1740ba23bfad22b8b0b",
+    "url": "/CheckboxDemo/static/media/e-UkraineHead-Medium.9fabd1e0.ttf"
+  },
+  {
+    "revision": "f1be010d6d38612394845336ca4d8bd9",
+    "url": "/CheckboxDemo/static/media/f1be010.f1be010d.svg"
+  },
+  {
+    "revision": "82014f62fb48815d79d52897f636f169",
+    "url": "/CheckboxDemo/static/media/gide.82014f62.svg"
+  },
+  {
+    "revision": "aafe12f34f6928a1318e6bb3cdc445ef",
+    "url": "/CheckboxDemo/static/media/gide.aafe12f3.png"
+  },
+  {
+    "revision": "bef2b910804523162a4e8ff878ae6a85",
+    "url": "/CheckboxDemo/static/media/gideS.bef2b910.png"
+  },
+  {
+    "revision": "798910589a97386104b1440704ad97a8",
+    "url": "/CheckboxDemo/static/media/goods.79891058.svg"
+  },
+  {
+    "revision": "de5e7d7700e7444658aea79939ece657",
+    "url": "/CheckboxDemo/static/media/goods.de5e7d77.png"
+  },
+  {
+    "revision": "3f5d73d383ea66b3b4ee7a31930bbe9b",
+    "url": "/CheckboxDemo/static/media/kashier.3f5d73d3.svg"
+  },
+  {
+    "revision": "63c7665a807ea3fd0cb7e922b51922fd",
+    "url": "/CheckboxDemo/static/media/kashier.63c7665a.png"
+  },
+  {
+    "revision": "107bfeb687ec506d4ac9ea2306e7bd66",
+    "url": "/CheckboxDemo/static/media/kass.107bfeb6.svg"
+  },
+  {
+    "revision": "b8d148ae3658d6a45527ad76178e4bfa",
+    "url": "/CheckboxDemo/static/media/kass.b8d148ae.png"
+  },
+  {
+    "revision": "8cb0f07fbe3980e0e07ea649113ed9a2",
+    "url": "/CheckboxDemo/static/media/outlet.8cb0f07f.png"
+  },
+  {
+    "revision": "a55cc9ede5c160d6c99e182d0409ff65",
+    "url": "/CheckboxDemo/static/media/outlet.a55cc9ed.svg"
+  },
+  {
+    "revision": "80484648270960dd990c0054a645b79a",
+    "url": "/CheckboxDemo/static/media/qwerty.80484648.svg"
+  },
+  {
+    "revision": "3d8b62698cc16c04c190a7338cfc9ec2",
+    "url": "/CheckboxDemo/static/media/qwerty1.3d8b6269.svg"
+  },
+  {
+    "revision": "66e42a86699fdf263d8183a8b0a75f55",
+    "url": "/CheckboxDemo/static/media/return.66e42a86.png"
+  },
+  {
+    "revision": "7794472c3fba0843d4e76bc3d3dbafd7",
+    "url": "/CheckboxDemo/static/media/return.7794472c.svg"
+  },
+  {
+    "revision": "4172d58fbeaacac288638c53a322fe11",
+    "url": "/CheckboxDemo/static/media/returnS.4172d58f.png"
+  },
+  {
+    "revision": "e2d709b53b2b5c33968074a6cd203358",
+    "url": "/CheckboxDemo/static/media/sael.e2d709b5.svg"
+  },
+  {
+    "revision": "9d58c25daae2d48ead23e36c8f9513be",
+    "url": "/CheckboxDemo/static/media/sale.9d58c25d.png"
+  },
+  {
+    "revision": "358bec1f64e5c4e627da962e8a23a39e",
+    "url": "/CheckboxDemo/static/media/taxes.358bec1f.svg"
+  },
+  {
+    "revision": "f817bd077b0e12ed04255307f5d62e2a",
+    "url": "/CheckboxDemo/static/media/taxes.f817bd07.png"
+  },
+  {
+    "revision": "58262b9b1a030de68c9b326a42760ae4",
+    "url": "/CheckboxDemo/static/media/x-report.58262b9b.png"
+  },
+  {
+    "revision": "22f20a990e6930c2d88fee1ce4a9c34f",
+    "url": "/CheckboxDemo/static/media/z-report.22f20a99.png"
+  }
+]);
